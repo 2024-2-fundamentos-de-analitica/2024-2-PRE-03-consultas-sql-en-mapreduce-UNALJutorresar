01e1a798c35dc72c9ee0f85ef9a6b81c0730e666
UNALJutorresar <jutorresar@unal.edu.co>
2024-11-13 09:08:06 -0500

    Delivery

--- homework/queries.py
+++ homework/queries.py
@@ -3,6 +3,26 @@
 # pylint: disable=broad-exception-raised
 # pylint: disable=import-error
 
+from mapreduce import run_mapreduce_job
+
+def mapper_query_1(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip() + ",tip_rate"))
+        else:
+            row_values = row.strip().split(",")
+            total_bill = float(row_values[0])
+            tip = float(row_values[1])
+            tip_rate = tip / total_bill
+            result.append((index, row.strip() + "," + str(tip_rate)))
+    return result
+
+
+def reducer_query_1(sequence):
+    """Reducer"""
+    return sequence
 
 #
 # ORQUESTADOR:
@@ -10,7 +30,12 @@
 def run():
     """Orquestador"""
 
+    run_mapreduce_job(
+        mapper=mapper_query_1,
+        reducer=reducer_query_1,
+        input_directory="files/input",
+        output_directory="files/query_1",
+    )
 
 if __name__ == "__main__":
-
     run()
